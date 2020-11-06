3f1194e9be837d369f2f939a49b0cce7973bc279
Sam Pasquesi <spasquesi19@gmail.com>
2020-11-06 17:23:37 -0600

    Train knn able to take several trainingSets

--- Classifier/knn.py
+++ Classifier/knn.py
@@ -8,34 +8,42 @@ class KNN(classify.Classifier):
     def __init__(self, n):
         self.model = KNeighborsClassifier(n_neighbors=n)
 
-    def train(self, trainingData):
+    def train(self, trainingSets):
+        # make four empty numpy arrays
+        X_train_all, X_test_all, y_train_all, y_test_all = np.array([[],[],[],[]])
+        for trainingData in trainingSets:
       	# trainingData -> (c0, c1, c2) -> (x_groups, y_groups, t_groups), l_groups
           #  -> each group is 1 sec w 190 rows
 
-        # channel_y: 3 channel list -> (? intervals, 190 recordings/interval)
-        channel_y_list = [np.stack(channel[0][1]) for channel in trainingData.values()]
+            # channel_y: 3 channel list -> (? intervals, 190 recordings/interval)
+            channel_y_list = [np.stack(channel[0][1]) for channel in trainingData.values()]
 
-        # channel_data: (? intervals, 190 readings/interval, 3 channels)
-        channel_data = np.stack(channel_y_list, axis=-1)
-        
-        # channel_means: (? meaned intervals, 3 channels)
-        channel_means = channel_data.mean(axis=1)
-        
-        train_size = int(0.7*channel_means.shape[0])
-        #means are normalized only on training data
-        #labels across channels should be identical
-        X = channel_means/channel_means[:train_size].std(axis=0)
-        y = trainingData[0][1]
-  
-        
-        X_train, X_test = np.split(X, [train_size])
-        y_train, y_test = np.split(y, [train_size])
+            # channel_data: (? intervals, 190 readings/interval, 3 channels)
+            channel_data = np.stack(channel_y_list, axis=-1)
+            
+            # channel_means: (? meaned intervals, 3 channels)
+            channel_means = channel_data.mean(axis=1)
+            
+            train_size = int(0.7*channel_means.shape[0])
+            #means are normalized only on training data
+            #labels across channels should be identical
+            X = channel_means/channel_means[:train_size].std(axis=0)
+            y = trainingData[0][1]
+    
+            
+            X_train, X_test = np.split(X, [train_size])
+            y_train, y_test = np.split(y, [train_size])
+
+            np.append(X_train_all, X_train)
+            np.append(X_test_all, X_test)
+            np.append(y_train_all, y_train)
+            np.append(y_test_all, y_test)
 
         
-        self.model.fit(X_train, y_train)
+        self.model.fit(X_train_all, y_train_all)
 
-        results = self.model.predict(X_test)
-        accuracy = (results==y_test).mean()
+        results = self.model.predict(X_test_all)
+        accuracy = (results==y_test_all).mean()
         
         print(accuracy)
 
--- Training/trainKnn.py
+++ Training/trainKnn.py
@@ -6,9 +6,11 @@ import Datacreate as dc
 import knn as model
 # import trainExample as trainer
 
-path = "../../Recordings/Fall_2020/OpenBCISession_2020-10-11_16-26-10-YAN-RIGHT-FOOT/OpenBCI-RAW-2020-10-11_16-26-50.txt"
-label_path = "../../Recordings/Labels/yanRightFoot"
+#path = "../../Recordings/Fall_2020/OpenBCISession_2020-10-11_16-26-10-YAN-RIGHT-FOOT/OpenBCI-RAW-2020-10-11_16-26-50.txt"
+path = "../../Recordings/Fall_2020/OpenBCISession_2020-10-11_16-33-50-YAN-LEFT-EYE/OpenBCI-RAW-2020-10-11_16-38-59.txt"
+#label_path = "../../Recordings/Labels/yanRightFoot"
+label_path = "../../Recordings/Labels/yanLeftEye"
 
 knn = model.KNN(3)
-observations = dc.getObservationSet(path, label_path, 1000, [0,1,2])
+observations = dc.getObservationSet(path, label_path, 1000, [0,1,2], 'R_EYE')
 knn.train(observations)

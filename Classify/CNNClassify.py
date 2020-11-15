909451860d0f69274d523c72e323a9c2a4e79784
Aaryaman Kumar <55060845+aaryamank@users.noreply.github.com>
2020-11-16 01:17:34 +0530

    Update CNNClassify.py

--- Classify/CNNClassify.py
+++ Classify/CNNClassify.py
@@ -19,33 +19,33 @@ class CNN(classify.Classifier):
         
         return X_train, X_val, y_train, y_val
         
-def convolute(X_train, X_val, y_train, y_val):
-    cnn3 = Sequential()
-    cnn3.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-    cnn3.add(MaxPooling2D((2, 2)))
-    cnn3.add(Dropout(0.25))
+    def convolute(X_train, X_val, y_train, y_val):
+        cnn3 = Sequential()
+        cnn3.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+        cnn3.add(MaxPooling2D((2, 2)))
+        cnn3.add(Dropout(0.25))
 
-    cnn3.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    cnn3.add(MaxPooling2D(pool_size=(2, 2)))
-    cnn3.add(Dropout(0.25))
+        cnn3.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+        cnn3.add(MaxPooling2D(pool_size=(2, 2)))
+        cnn3.add(Dropout(0.25))
 
-    cnn3.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    cnn3.add(Dropout(0.4))
+        cnn3.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+        cnn3.add(Dropout(0.4))
 
-    cnn3.add(Flatten())
+        cnn3.add(Flatten())
 
-    cnn3.add(Dense(128, activation='relu'))
-    cnn3.add(Dropout(0.3))
-    cnn3.add(Dense(10, activation='softmax'))
+        cnn3.add(Dense(128, activation='relu'))
+        cnn3.add(Dropout(0.3))
+        cnn3.add(Dense(10, activation='softmax'))
 
-    cnn3.compile(loss=keras.losses.categorical_crossentropy,
-                  optimizer=keras.optimizers.Adam(),
-                  metrics=['accuracy'])
-    
-    trained = cnn1.fit(X_train, y_train,
-          batch_size=256,
-          epochs=10,
-          verbose=1,
-          validation_data=(X_val, y_val))
+        cnn3.compile(loss=keras.losses.categorical_crossentropy,
+                      optimizer=keras.optimizers.Adam(),
+                      metrics=['accuracy'])
 
-    return trained
+        trained = cnn1.fit(X_train, y_train,
+              batch_size=256,
+              epochs=10,
+              verbose=1,
+              validation_data=(X_val, y_val))
+
+        return trained

9ca9d49986691bc1e39329a85d00ed15527ac728
Prithvi Dharmaraj <prithvidraj@gmail.com>
2022-03-23 20:17:38 -0500

    Added logic for additional car actions

--- RC_Controller/Controller.py
+++ RC_Controller/Controller.py
@@ -6,9 +6,16 @@ class Controller():
 	def __init__(self, serial_speed, serial_port):
 		self.serial_speed = serial_speed
 		self.serial_port = serial_port
-	
+		self.moving_forward = False
+		self.moving_backward = False
+		self.turning = False
 	def connect(self):
 		self.ser = serial.Serial(self.serial_port, self.serial_speed, timeout=1)
+		if(self.ser.isOpen() == False):
+			print("opening port")
+			self.ser.open()
+		else:
+			print("Already opened")
 
 	def close(self):
 		self.ser.close()
@@ -17,22 +24,53 @@ class Controller():
 	def sendAction(self, action):
 		actionInt = np.argmax(action)
 		actionLabel = "S"
-		if actionInt == 3: # L_EYE
-			actionLabel = "U"
-		elif actionInt == 4: # R_EYE
-			actionLabel = "D"
-		
+		if actionInt == 1: # L_EYE
+			self.sendStopAction()
+			actionLabel = "R"
+			self.turning = True
+		elif actionInt == 2: # R_EYE
+			self.sendStopAction()
+			actionLabel = "L"
+			self.turning = True
+
+		elif actionInt == 3: # JAW_CLENCH
+			if (self.moving_backward):
+				self.sendStopAction()
+			if (self.moving_forward):
+				actionLabel = "S"
+				self.moving_forward = False
+			else:
+				actionLabel = "U" 
+				self.moving_forward = True
+
+		elif actionInt == 4: # EYEBROW_RAISE
+			if (self.moving_forward):
+				self.sendStopAction()
+			if (self.moving_backward):
+				actionLabel = "S"
+				self.moving_backward = False
+			else:
+				actionLabel = "D"
+				self.moving_backward = True
+
+		print("Sending %s" % actionLabel)
 		self.ser.write(bytes(actionLabel, 'utf-8'))
-		print("recieving message from arduino ...")
+		print("receiving message from arduino ...")
 		res = self.ser.readline()
 		if (res != ""):
 			print("arduino says: %s" % res)
 		else:
 			print("arduino doesnt respond")
 
-		time.sleep(.5)
-
+		#time.sleep(.5)
+		if (self.turning):
+			time.sleep(0.5)
+			self.sendStopAction()
+			self.turning = False
 		# STOP THE ACTION 
+		#self.sendStopAction()
+	
+	def sendStopAction(self):
 		self.ser.write(b'S')
 		print("recieving message from arduino ...")
 		res = self.ser.readline()
@@ -40,3 +78,6 @@ class Controller():
 			print("arduino says: %s" % res)
 		else:
 			print("arduino doesnt respond")
+
+	def getSer(self):
+		return self.ser
